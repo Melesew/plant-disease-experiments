49d256fc42f3b1a8dc23433422bee107bee3ec5b
= <=>
2018-10-31 21:29:32 +0300

    model_eval added

---
+++ model_eval/confussion_matrix.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+from keras.models import load_model
+from PIL import Image
+from keras.preprocessing import image
+import subprocess
+from keras.applications.inception_v3 import preprocess_input
+import argparse
+
+parser = argparse.ArgumentParser()
+
+
+#constants
+model_path = os.path.join('../Plant_Disease_Detection_Benchmark_models/Models', 'VGG_scratch_94.h5')
+model = load_model(model_path)
+
+data_path = "/home/icog/Mele/generated_data/"
+
+
+target_size = (64, 64)
+
+def predict(img_path):
+    array = [0] * 38
+    for f in os.listdir(img_path):
+        if f.startswith('.'): #path all .gits
+            continue
+        img = Image.open(img_path + f)
+        if img.size != target_size:
+            img = img.resize(target_size)
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        
+        preds = model.predict(x)
+        label = np.argmax(preds)
+        result = str(label)
+        
+        idx = int(result)
+        array[idx] += 1
+
+    return array
+
+if __name__ == "__main__":
+    file = open("confussion_matrix_4_generated.txt", "a")
+    image_path = os.listdir(data_path)
+    image_path.sort()
+    for path in image_path:
+        if path.startswith('.'): # to pass .git folder
+            continue
+        matrix_array = predict(data_path+path+'/')
+        print(matrix_array)
+        file.write(str(matrix_array) + '\n')
+    file.close()
+        
+    
